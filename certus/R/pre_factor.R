41e806719d7e39dc8152acce52032e01be1471eb
laurentj2 <laurent.pcupdate@gmail.com>
2015-06-06 14:43:22 +0200

    ggddg
    gdsggsd

--- certus/R/pre_factor.R
+++ certus/R/pre_factor.R
@@ -4,7 +4,7 @@ test=round(runif(1),10)
 test=substring(test, 3,10)
 plot.pre_factor <- reactive(function(){
   setwd("/srv/shiny")
-  source("https://www.dropbox.com/s/j4cshlyclih2car/wrap.r?dl=1")
+  source("http://www.dropbox.com/s/j4cshlyclih2car/wrap.r?dl=1")
     data = getData()
     
     tryCatch({							
