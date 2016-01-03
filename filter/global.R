40f3ab0f3399a217b90e93e67c52a94a614af698
laurentj2 <laurent.pcupdate@gmail.com>
2016-01-03 01:45:56 +0100

    dd

--- filter/global.R
+++ filter/global.R
@@ -13,10 +13,6 @@ con = gzcon(file('../sit', 'rb'))
 close(con)
 
 
-
-
-
-
 load.packages('quantmod')
 if (!require(quantmod)) {
 	stop("This app requires the quantmod package. To install it, run 'install.packages(\"quantmod\")'.\n")
